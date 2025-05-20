a880b4ce13d6e60d255ec82a5ace9d7906c9cebb
Collin Maillet <cmaillet@lcs.on.ca>
2025-05-20 11:26:21 -0400

    Initial Commit

---
+++ PocketQR/PocketQRApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketQRApp.swift
+//  PocketQR
+//
+//  Created by Collin Maillet on 2025-05-20.
+//
+
+import SwiftUI
+
+@main
+struct PocketQRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
